b5762415cec9c1231ce867dc267379c5682658ab
Mustafa Camurcu <camurcu.m@husky.neu.edu>
2017-01-10 10:00:15 -0800

    Initial Commit

---
+++ Wizardry/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Wizardry
+//
+//  Created by Mustafa Camurcu on 1/10/17.
+//  Copyright © 2017 Mustafa Camurcu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
